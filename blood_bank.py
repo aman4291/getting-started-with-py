592d920f0d6bdca2dea97e4565d29b43a0288c09
Aman <amanmahi0307@gmail.com>
2022-08-02 12:35:21 +0530

    created inventory

---
+++ blood_bank.py
@@ -0,0 +1,6 @@
+from unicodedata import name
+print("Welcome to the blood bank inventory sir/madam \n")
+# created data inventory-----
+
+data_inventory=['Aman', '2000', 'Indore', 'A+', '23 march 2020', 'Annant', '1994', 'Banglore', 'B+', '7 july 2021', 'Pratik', '2001', 'Mangliya', 'A', 'Never', 'Sampada', '2002', 'Texas', 'B+', '14 november 2018', 'Krati', '2002', 'Mandsaur', 'O+', '3 november 2020']
+
