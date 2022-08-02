83409788e85a2bd2bed07ed1c59c220d260babc8
Aman <amanmahi0307@gmail.com>
2022-08-02 12:40:39 +0530

    taking inputs from user

--- blood_bank.py
+++ blood_bank.py
@@ -4,3 +4,11 @@ print("Welcome to the blood bank inventory sir/madam \n")
 
 data_inventory=['Aman', '2000', 'Indore', 'A+', '23 march 2020', 'Annant', '1994', 'Banglore', 'B+', '7 july 2021', 'Pratik', '2001', 'Mangliya', 'A', 'Never', 'Sampada', '2002', 'Texas', 'B+', '14 november 2018', 'Krati', '2002', 'Mandsaur', 'O+', '3 november 2020']
 
+# ---------To take inputs from users---------
+
+def data_input():
+    name = input("\nEnter your Name:\n")
+    dob= input("Enter your Date of birth:\n")
+    place= input("Where do you live?\n")
+    blood_group= input("What is your Blood Group:\n")
+    last_donated_date= input("Last time you donated blood(specify date):\n")
